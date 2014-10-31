8e642b78150279791018abb10c71170d4b7273c6
Sebastián Valencia <sc.valencia606@gmail.com>
2014-10-30 21:24:30 -0500

    Chess

---
+++ Tutorial/03. Standard I:O and control/chessBoard.pas
@@ -0,0 +1,12 @@
+program chessBoard(input, output);
+const
+	TIMES = 4;
+var
+	i : integer;
+begin
+	for i := 1 to TIMES do
+	begin
+		writeln('B W B W B W B W');
+		writeln('W B W B W B W B');
+	end;
+end.
