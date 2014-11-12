34cd77e5eec280ee72fb484da6e0b83e35217161
Sebastián Valencia <sc.valencia606@gmail.com>
2014-11-11 22:46:25 -0500

    Enumeration types

---
+++ Tutorial/04. Structured types/enums.pas
@@ -0,0 +1,12 @@
+program enums(input, output);
+type
+	names = (eduardo, sebastian, carlos, valeria, gerardo, marcela, camilo, andres, alicia);
+var
+	name : names;
+begin
+	for name := eduardo to alicia do 
+		writeln(ord(name), ' : ', name);
+	writeln(eduardo < alicia);
+	writeln(alicia < eduardo);
+	writeln(true > false); (* Boolean is an enum type*)
+end.
