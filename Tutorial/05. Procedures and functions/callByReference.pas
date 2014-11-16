7cf877a11c696adb1f02ec17ca48cbd3d3d29957
Sebastián Valencia <sc.valencia606@gmail.com>
2014-11-16 15:20:03 -0500

    Call by reference

---
+++ Tutorial/05. Procedures and functions/callByReference.pas
@@ -0,0 +1,21 @@
+program callbyreference(input, output);
+var
+	x : integer;
+
+procedure change;
+begin
+	x := x + 1;	
+end;
+
+begin
+	x := 0;
+	writeln(x);
+	change;
+	writeln(x);
+	change;
+	writeln(x);
+	change;
+	writeln(x);
+	change;
+	writeln(x);
+end.
