4b6f0907755f3634cc1b35891f428d95b2c47072
Sebastián Valencia <sc.valencia606@gmail.com>
2014-12-19 02:00:41 -0500

    New exercise

--- Tutorial/05. Procedures and functions/daysOfLife.pas
+++ Tutorial/05. Procedures and functions/daysOfLife.pas
@@ -12,11 +12,70 @@ type
 	end;
 
 	function daysInMonth(month, year : integer) : integer;
-	var
-		isLeap : boolean;
 	begin
-		isLeap := isLeapYear(year);
+		if isLeapYear(year) and (month = 2) 
+			then daysInMonth := 29
+		else if month = 1 
+			then daysInMonth := 31
+		else if month = 2 	
+			then daysInMonth := 28
+		else if month = 3 
+			then daysInMonth := 31
+		else if month = 4 
+			then daysInMonth := 30
+		else if month = 5 
+			then daysInMonth := 31
+		else if month = 6 
+			then daysInMonth := 30
+		else if month = 7 
+			then daysInMonth := 31
+		else if month = 8 
+			then daysInMonth := 31
+		else if month = 9 
+			then daysInMonth := 30
+		else if month = 10 
+			then daysInMonth := 31
+		else if month = 11 
+			then daysInMonth := 30
+		else if month = 12 
+			then daysInMonth := 31
 	end;
+
+	function daysUntilNextYear(day, month, year : integer) : integer;
+	var
+		ans, i : integer;
+	begin
+		ans := daysInMonth(month, year) - day;
+		i := month;
+		while i < 12 do begin
+			ans := ans + daysInMonth(i, year);
+			i := succ(i);
+		end;
+		daysUntilNextYear := ans
+	end;
+
+	function funnyNumber(day, month, year : integer) : integer;
+	begin
+		month := (month + 9) mod 12;
+		year := year - (month div 10);
+		funnyNumber := (365 * year) + (year div 4) - (year div 100) + 
+			(year div 400) + (month * 306 + 5) div 10 + (day - 1);
+	end;
+
+	function daysBetweenDates(d1, m1, y1, d2, m2, y2 : integer) : integer;
+	begin
+		daysBetweenDates := funnyNumber(d2, m2, y2) - funnyNumber(d1, m1, y1)
+	end;
+
+	function secondsInDays(days : integer) : integer;
+	const
+		SECONDS_MINUTE = 60;
+		MINUTES_HOUR = 60;
+		HOUR_DAYS = 24;
+	begin
+		secondsInDays := days * HOUR_DAYS * MINUTES_HOUR * SECONDS_MINUTE;		
+	end;
+
 begin
-	writeln(isLeapYear(1900));
+	writeln(secondsInDays(daysBetweenDates(1, 2, 1993, 1, 1, 1994)));
 end.
