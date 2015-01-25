2eea34942a22d3c9b6f557ecda70470580d0f45e
Sebastián Valencia <sc.valencia606@gmail.com>
2015-01-24 20:21:40 -0500

    Failed atoi attempt in pascal

--- Tutorial/04. Structured types/atoi.pas
+++ Tutorial/04. Structured types/atoi.pas
@@ -1,22 +1,111 @@
-program atoi(input, output);
+program stringHandling(input, output);
 
-	const
-		MAX_LEN = 32;
-		RADIX = 10;
+	type
+		str = array[0..20] of char;
+		{digits = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');}
 
 	var
-		jp, kp : 0 .. MAX_LEN;
-		buffer : array [1 .. MAX_LEN] of char;
-		num : integer;
+		test : str;
+
+	function lengthString(var reference : str) : integer;
+
+		var
+			i : 0 .. 20;
+
+	begin
+
+		i := 0;
+		while reference[i] <> '#' do
+			i := i + 1;
+
+		lengthString := i - 1
+
+	end;
+
+	procedure initializeString(var reference : str);
+
+		var
+			i : 0 .. 20;
+
+	begin
+
+		for i := 0 to 20 do
+			reference[i] := '#';
+
+	end;
+
+	procedure printString(var reference : str);
+
+		var
+			i, len : 0 .. 20;
+
+	begin
+
+		len := lengthString(reference);
+		for i := 0 to len do
+			write(reference[i]);
+		writeln;
+
+	end;
+
+	function power(n, b : integer) : integer;
+
+		var
+			ans, i : integer;
+
+	begin
+
+		ans := 1;
+		for i := 1 to b do
+			ans := ans * n;
+
+		power := ans;
+
+	end;
+
+	function atoi(var reference : str) : integer;
+
+		var
+			i : 0 .. 20;
+			ans, len, rep, p : integer;
+	begin
+
+		len := lengthString(reference);
+		ans := 0; rep := 0;
+		i := len;
+
+		while i <> 0 do begin
+
+			rep := ord(reference[i]) - 48;			
+			p := len - i;
+
+			writeln(i, ' ', reference[i], ' ', rep, ' ', p);
+
+			ans := ans + rep * power(10, p);
+			i := i - 1;			
+		end;
+
+		atoi := ans;
+
+	end;
 
 begin
 
-	kp := 1;
+	initializeString(test);
 
-	while buffer[kp] <> '0' do begin
-		read(buffer[kp]);
-		writeln(buffer[kp]);
-		kp := kp + 1;
-	end;
+	test[0] := 'N'; test[1] := 'i'; test[2] := 'k';
+	test[3] := 'l'; test[4] := 'a'; test[5] := 'u';
+	test[6] := 's'; test[7] := ' '; test[8] := 'W';
+	test[9] := 'i'; test[10] := 'r'; test[11] := 't';
+	test[12] := 'h'; test[13] := '.';
+
+	initializeString(test);
+
+	test[0] := '1'; test[1] := '0';
+
+
+	printString(test);
+
+	writeln(atoi(test));
 
 end.
